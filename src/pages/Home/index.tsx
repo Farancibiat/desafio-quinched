af0de2aa1d53203795bb0cd946acbe435ca45782
Farancibiat <arancibiat.felipe@gmail.com>
2024-10-03 18:29:55 -0300

    add mejora visual y texto con nombre legal del club para Google Non profits

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -35,73 +35,185 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="homepage-background">
-       <Image src={header} alt="header" className="header" />
-      <div className='logo-top'>
-        <Image src={logo} alt="Club Logo" className="club-logo" height={80} width={80} />
-      </div>
-      <h1>Tiempo faltante para la largada:</h1>
-      <div className="timer">
-        {timeLeft && (
-          <Box
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          gap={2}
-          bgcolor="rgb(86,165,170)" // Azul oscuro de fondo
-          color="rgb(255,253,22)" // Amarillo fosforescente
-          p={3}
-          borderRadius="12px"
-        >
-          <Box textAlign="center">
-            <Typography variant="h4" sx={{ color: 'rgb(255,253,22)' }}>{timeLeft.days}</Typography>
-            <Typography variant="subtitle1">Días</Typography>
-          </Box>
-          <Box textAlign="center">
-            <Typography variant="h4" sx={{ color: 'rgb(255,253,22)' }}>{timeLeft.hours}</Typography>
-            <Typography variant="subtitle1">Horas</Typography>
-          </Box>
-          <Box textAlign="center">
-            <Typography variant="h4" sx={{ color: 'rgb(255,253,22)' }}>{timeLeft.minutes}</Typography>
-            <Typography variant="subtitle1">Minutos</Typography>
-          </Box>
-          <Box textAlign="center">
-            <Typography variant="h4" sx={{ color: 'rgb(255,253,22)' }}>{timeLeft.seconds}</Typography>
-            <Typography variant="subtitle1">Segundos</Typography>
-          </Box>
-        </Box>
-        //   <>
-
-        //   <span className="hours">{String(timeLeft?.days).padStart(2, '0')}</span>
-        //   <span className="separator">:</span>
-        //   <span className="hours">{String(timeLeft?.hours).padStart(2, '0')}</span>
-        //   <span className="separator">:</span>
-        //   <span className="minutes">{String(timeLeft?.minutes).padStart(2, '0')}</span>
-        //   <span className="separator">:</span>
-        //   <span className="seconds">{String(timeLeft?.seconds).padStart(2, '0')}</span>
-        // </>
-      
-      )}
-      </div>
-
-      <div className="downloads mt-8">
-        <h1>No olvides leer los siguientes documentos:</h1>
-        <Button><form action="https://storage.googleapis.com/feriaaac/publicos/seguridad.pdf" >
-          <input type="submit" value="Plan de Seguridad" />
-        </form></Button>
-
-        <Button>
-          <form action="https://storage.googleapis.com/feriaaac/publicos/bases.pdf" >
-            <input type="submit" value="Bases" />
-          </form></Button>
-
-        <div className="register mt-8">
-          <Button variant="contained" color="success">
-            <form action="https://forms.gle/8dc88mRCDcd1VafQ7" target="_blank" rel="noopener noreferrer">
-              <input type="submit" value="!Inscribete!" />
-            </form></Button>
+    <div className="homepage ">
+      <div className='grid grid-cols-12 gap-4 bg-teal-100 shadow '>
+        <div className='logo-top col-span-2 flex items-center'>
+          <Image src={logo} alt="Club Logo" className="club-logo" width={80} />
+        </div>
+        <div className="col-span-1"></div>
+        <div className='col-span-7 flex items-center'>
+          <Image src={header} alt="header" className="mx-0 shadow-lg rounded-md my-auto" height={150} />
         </div>
       </div>
+
+
+
+      <section className="bg-center bg-no-repeat bg-teal-400 bg-blend-multiply ">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 mb-8">
+          <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8 p-0" >
+
+            <h1 className="text-gray-900 dark:text-white text-2xl md:text-4xl font-bold mb-2">Tiempo faltante para la largada:</h1>
+            <div className="timer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 100 100"
+                width="100"
+                height="100">
+                {/* <!-- Reloj circular --> */}
+                <circle
+                  cx="50"
+                  cy="50"
+                  r="45"
+                  stroke="black"
+                  stroke-width="5"
+                  fill="none" />
+
+                {/* <!-- Aguja de la hora --> */}
+                <line
+                  x1="50"
+                  y1="50"
+                  x2="50"
+                  y2="30"
+                  stroke="black"
+                  stroke-width="3"
+                  stroke-linecap="round" />
+
+                {/* <!-- Aguja del minuto --> */}
+                <line
+                  x1="50"
+                  y1="50"
+                  x2="70"
+                  y2="50"
+                  stroke="black"
+                  stroke-width="2"
+                  stroke-linecap="round" />
+
+                {/* <!-- Centro del reloj --> */}
+                <circle
+                  cx="50"
+                  cy="50"
+                  r="2"
+                  fill="black" />
+              </svg>
+              <Box
+                className='mt-2'
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                gap={2}
+                bgcolor="rgb(86,165,170)" // Azul oscuro de fondo
+                color="rgb(255,253,22)" // Amarillo fosforescente
+                p={3}
+                borderRadius="12px"
+              >
+                <Box textAlign="center">
+                  <Typography variant="h4" className='text-3xl md:text-5xl' sx={{ color: 'rgb(255,253,22)' }}>{timeLeft ? timeLeft.days : 0}</Typography>
+                  <Typography variant="subtitle1">Días</Typography>
+                </Box>
+                <Box textAlign="center">
+                  <Typography variant="h4" className='text-3xl md:text-5xl' sx={{ color: 'rgb(255,253,22)' }}>{timeLeft ? timeLeft.hours : 0}</Typography>
+                  <Typography variant="subtitle1">Horas</Typography>
+                </Box>
+                <Box textAlign="center">
+                  <Typography variant="h4" className='text-3xl md:text-5xl' sx={{ color: 'rgb(255,253,22)' }}>{timeLeft ? timeLeft.minutes : 0}</Typography>
+                  <Typography variant="subtitle1">Minutos</Typography>
+                </Box>
+                <Box textAlign="center">
+                  <Typography variant="h4" className='text-3xl md:text-5xl' sx={{ color: 'rgb(255,253,22)' }}>{timeLeft ? timeLeft.seconds : 0}</Typography>
+                  <Typography variant="subtitle1">Segundos</Typography>
+                </Box>
+              </Box>
+
+            </div>
+            <Button className="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900" variant="contained" color="success">
+              <form action="https://forms.gle/8dc88mRCDcd1VafQ7" target="_blank" rel="noopener noreferrer">
+                <input type="submit" value='¡ Inscribete !' />
+
+              </form></Button>
+          </div>
+          <div className="grid md:grid-cols-2 gap-8">
+            <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+              <a href="#" className="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 mb-2">
+                <svg className="w-2.5 h-2.5 me-1.5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 100 100"
+                  width="100"
+                  height="100"
+                  stroke="currentColor">
+
+                  <circle
+                    cx="50"
+                    cy="80"
+                    r="5"
+                    stroke="currentColor"
+                  />
+
+        
+                  <line
+
+                    x1="50"
+                    y1="60"
+                    x2="50"
+                    y2="20"
+                    stroke="currentColor"
+                    stroke-width="8"
+                    stroke-linecap="round" />
+                </svg>
+                Importante
+              </a>
+              <h2 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">Revisas las Bases 2024</h2>
+              <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Te recomendamos leer detenidamente las bases, para evitar problemas de último minuto. En ellas encontrarás los recorridos, plan de contingencia y otros.</p>
+              <a href="https://storage.googleapis.com/feriaaac/publicos/bases.pdf" className="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Ver Documento
+                <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                </svg>
+              </a>
+            </div>
+
+            <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+              <a href="#" className="bg-yellow-100 text-yellow-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-yellow-500 mb-2">
+                <svg className="w-5 h-5 me-1.5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100" height="100" fill="currentColor">
+                  <path d="M50 5 L75 20 L75 60 Q50 90 25 60 L25 20 Z" stroke="currentColor" stroke-width="5" fill="none" />
+                  <path d="M35 50 L45 60 L65 40" stroke="currentColor" stroke-width="5" fill="none" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+                Seguridad
+              </a>
+              <h2 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">Revisa el plan de Contingencia</h2>
+              <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Es nuestro compromiso asegurar que todos quienes participen del evento cuenten con el mayor resguardo, por lo que ponemos a disposición de todos los competidores el plan de contingencia 2024.</p>
+              <a href="https://storage.googleapis.com/feriaaac/publicos/seguridad.pdf" className="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Ver Plan
+                <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className=" p-2 bg-teal-600 border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">asdasd</section>
+
+      <footer className="fixed bottom-0 left-0 z-20 w-full p-2 bg-teal-600 border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600 ">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span className="text-sm  sm:text-center dark:text-gray-400">© 2024 <a href="https://www.aguasabiertaschiloe.com/" className="hover:underline">Club Deportivo Social y Cultural Aguas Abiertas Chiloé</a>.
+          </span>
+           <span>
+           <a href="https://www.farancibiat.cl/" className="hover:underline text-xs">Sitio desarrollado por Farancibiat</a>
+           </span>
+          {/* <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+            <a href="#" className="hover:underline me-4 md:me-6">About</a>
+        </li>
+        <li>
+            <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+        </li>
+        <li>
+            <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+        </li>
+        <li>
+            <a href="#" className="hover:underline">Contact</a>
+        </li>
+    </ul> */}
+        </div>
+      </footer>
     </div>
   );
 };
