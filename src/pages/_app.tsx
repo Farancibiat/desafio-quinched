736d6d5ff5d929375971169d2be8ed6eddcc3b58
Farancibiat <arancibiat.felipe@gmail.com>
2024-10-09 21:27:14 -0300

    Add Google Analytics 4 tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import "@/styles/globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+  <Component {...pageProps} />;
+  <GoogleAnalytics gaId="G-TG3KHNHRKJ" />
+  </>
 }
